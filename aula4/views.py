a3642fc61171aaf212b34a3c0a5d5c753ef4a0b6
eduhenlima <eduardoh.lima17@hotmail.com>
2020-02-29 13:07:54 -0300

    listando parametros

--- aula4/views.py
+++ aula4/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "alunos":[
+            "Joao",
+            "Maria",
+            "Jose",
+            "Pedro",
+        ]
+    }
+    return render(request, 'aula4/index.html', context=context)
